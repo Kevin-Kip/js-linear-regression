e4565d449527422e85cf5b858d8349eca3bf920c
Kevin-Kip <masterfork5@gmail.com>
2018-08-03 22:40:34 +0300

    Initial commit

---
+++ js/linear-regression.js
@@ -0,0 +1,15 @@
+/*
+    Basic formula
+    y = mx + b
+    where:
+        x is the input
+        m is the slope of the line
+        and b is the y-intercept
+
+    m = (sum of (eachX - meanOfX)*(eachY - sumOfY)) / square(sum of (eachX - meanOfX))
+
+    b = meanOfY - (m * meanOfX)
+*/
+
+
+
