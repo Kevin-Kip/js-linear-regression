09da4add9a6fb34d8b27b14e41362a1cb98e4321
Kevin-Kip <masterfork5@gmail.com>
2018-08-05 16:14:11 +0300

    Added code for Ordinary Least Squares method

--- js/linear-regression.js
+++ js/linear-regression.js
@@ -6,7 +6,7 @@
         m is the slope of the line
         and b is the y-intercept
 
-    m = (sum of (eachX - meanOfX)*(eachY - sumOfY)) / square(sum of (eachX - meanOfX))
+    m = (sum of (eachX - meanOfX)*(eachY - sumOfY)) / sum of(square of (eachX - meanOfX))
 
     b = meanOfY - (m * meanOfX)
 */
